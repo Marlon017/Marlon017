93eee722658e1088e1cb7c9d1721642b91c7eacb
marlo <marlo@LAPTOP-1K437R6P>
2021-04-15 16:12:52 +0200

    trabajo de entornos

---
+++ src/Proyectogithub/programa.java
@@ -0,0 +1,24 @@
+package Proyectogithub;
+import java.util.Scanner;
+public class programa {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		String contraseña="123";
+		String cadena="";
+
+		for(int i=1;i<=3;i++){
+			System.out.println("Contraseña:");
+			cadena=entrada.nextLine();
+			if(contraseña.equals(cadena)) {
+				System.out.println("Enhorabuena");
+				i=4;
+			}
+			if(i==3) {
+				System.out.println("Usuario bloqueado");
+			}
+		}
+		
+		System.out.println("Programa finalizado");
+	}
+}
