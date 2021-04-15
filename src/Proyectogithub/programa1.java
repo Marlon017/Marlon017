300f97a31391f5d6c3d5e491c8cc7d22fe29ae80
marlo <marlo@LAPTOP-1K437R6P>
2021-04-15 18:15:55 +0200

    otra modificacion 1

--- src/Proyectogithub/programa1.java
+++ src/Proyectogithub/programa1.java
@@ -1,9 +1,6 @@
 package Proyectogithub;
 
 public class programa1 {
-<<<<<<< HEAD
 	int h;
-=======
-  System.out.println("Añadiendo contenido al programa1");
->>>>>>> branch 'master' of https://github.com/Marlon017/Marlon017.git
+
 }
